5d1753cff0a215acc07fe529a8e626db6bc60aa8
Hack Reactor Students <accounts@hackreactor.com>
2014-12-16 20:51:40 -0800

    blayd

--- src/HyphyDancer.js
+++ src/HyphyDancer.js
@@ -1,7 +1,7 @@
 var HyphyDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps);
 
-  this.$node = $("<div class='turtles'>  <img src='src/gifs/giphy.gif' alt='turtles'</div>");//add gif class here
+  this.$node = $("<span class='turtles'>  <img src='src/gifs/giphy.gif' alt='turtles'</span>");//add gif class here
   //add .css properties here
   this.$node.css({top : top, left : left});
   this.timeBetweenSteps = timeBetweenSteps;
@@ -13,7 +13,14 @@ HyphyDancer.prototype.constructor = HyphyDancer;
 HyphyDancer.prototype.step = function(){
 
   Dancer.prototype.step.call(this);
-
+  if (this.blueBall === true){
+    this.$node.css({border :'10px solid pink'});
+    this.blueBall = false;
+  }
+  else{
+    this.$node.css({border :'10px solid blue'});
+    this.blueBall = true;
+  }
   this.$node;
 };
 
--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -6,7 +6,7 @@ var BlinkyDancer = function(top, left, timeBetweenSteps){
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  // console.log($node);
+  console.log($node);
 
 
 };
